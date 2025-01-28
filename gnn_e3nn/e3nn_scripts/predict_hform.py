953afb95cc0ca18c043171cda8f251677041327e
ronanglee <ronan54@hotmail.com>
2025-01-28 07:57:27 +0100

    minor changes

--- gnn_e3nn/e3nn_scripts/predict_hform.py
+++ gnn_e3nn/e3nn_scripts/predict_hform.py
@@ -100,7 +100,7 @@ model.to(device)
 model.eval()
 losses = []
 
-with open('predictions.csv', 'w', newline='') as csvfile:
+with open('hform_predictions.csv', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
     csv_writer.writerow(["id", "hform"])
 
